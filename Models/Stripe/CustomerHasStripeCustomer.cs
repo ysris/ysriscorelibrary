4e2272f747c545bb6c5cb109371d7f92a5cc048e
yoann <yoann@ysris.ch>
2018-02-11 17:38:38 +0100

    updated stripe service core part

--- Models/Stripe/CustomerHasStripeCustomer.cs
+++ Models/Stripe/CustomerHasStripeCustomer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,9 @@ namespace YsrisCoreLibrary.Models.Stripe
 {
     public class CustomerHasStripeCustomer
     {
+        [Key]
         public int customerId { get; set; }
+
         public string stripeCustomerId { get; set; }
     }
 }
