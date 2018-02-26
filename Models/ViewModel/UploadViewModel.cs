70bbc8b6b1529408810191e63444cae4a21483fc
yoann <yoann@ysris.ch>
2018-02-26 18:25:46 +0100

    added upload abstraction

---
+++ Models/ViewModel/UploadViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YsrisCoreLibrary.Models.ViewModel
+{
+    public class UploadViewModel<T> where T : class
+    {
+        public IFormFile file { get; set; }
+        public T entity { get; set; }
+    }
+}
