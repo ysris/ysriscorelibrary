e22cc2f21842d5648c6a49382f96ba72dceb91c7
yoann <yoann@ysris.ch>
2018-03-01 10:38:07 +0100

    added activation JS stuff

---
+++ wwwroot/bobos_components/controllers/ActivateCustomerInvitationController.js
@@ -0,0 +1,17 @@
+﻿angular.module("frontendAngularClientApp")
+    .factory("customerActivationService", function ($http, Upload) {
+        return {
+            inviteCustomer: function (entity) { return $http.post("/api/customer/activateinvitation", entity); }
+        };
+    })
+    .controller("ActivateCustomerInvitationController", function ($scope, $state, $rootScope, $stateParams, customerActivationService) {
+        $scope.entity = { email: $state.params.email, activationcode: $state.params.activationcode };
+
+        $scope.submit = function () {
+            customerActivationService.inviteCustomer($scope.entity).then(function (resp) {
+                $rootScope.addNotification('Activated');
+                $state.go("signin2");
+            });
+        }
+    })
+    ;
