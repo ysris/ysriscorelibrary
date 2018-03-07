f2e383784ee638d52b2388efe3fbcd8e8353d1d8
yoann <yoann@ysris.ch>
2018-03-07 04:18:04 +0100

    Adedd Yooz financial entities

---
+++ Models/Yooz/YoozCustomer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YsrisCoreLibrary.Models.Yooz
+{
+    [DataContract]
+    public class YoozCustomer
+    {
+        [Key]
+        [DataMember]
+        public int index { get; set; }
+
+        [DataMember]
+        public string code { get; set; }
+
+        [DataMember]
+        public string name { get; set; }
+    }
+}
