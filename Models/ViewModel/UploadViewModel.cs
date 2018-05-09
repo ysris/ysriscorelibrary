d4961f7462742ce3c5a724966d7a3dfa6f8d42e1
yoann <yoann@ysris.ch>
2018-05-09 09:52:01 +0200

    refactoreduploadviewmodel

--- Models/ViewModel/UploadViewModel.cs
+++ Models/ViewModel/UploadViewModel.cs
@@ -7,6 +7,10 @@ using System.Threading.Tasks;
 
 namespace YsrisCoreLibrary.Models.ViewModel
 {
+    /// <summary>
+    /// Wrapper for a post/upload in the same time call from client part
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public class UploadViewModel<T> where T : class
     {
         public IFormFile file { get; set; }
