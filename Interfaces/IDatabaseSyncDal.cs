213e6fc3a1236d51aba9e3527f2298babc66fbb7
yoann <yoann@ysris.ch>
2018-05-22 16:12:59 +0200

    Added IdatabaseSyncDal

---
+++ Interfaces/IDatabaseSyncDal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YsrisCoreLibrary.Interfaces
+{
+    public interface IDatabaseSyncDal
+    {
+        Task syncAll();
+        void _importYoozXml();
+        void _callYoozUpdater();
+        void SynchronizeWinBizCustomer();
+        void SynchronizeYoozCustomer();
+    }
+}
