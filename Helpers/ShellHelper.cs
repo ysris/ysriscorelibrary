a634dc7ca3b1d83569b587a6bccbd8a605fb6208
yoann <yoann@ysris.ch>
2018-05-22 16:11:21 +0200

    Added shell helper

---
+++ Helpers/ShellHelper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YsrisCoreLibrary.Helpers
+{
+    public static class ShellHelper
+    {
+        // cf https://loune.net/2017/06/running-shell-bash-commands-in-net-core/
+        public static string Bash(string cmd)
+        {
+            var escapedArgs = cmd.Replace("\"", "\\\"");
+
+            var process = new Process()
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    //FileName = "/bin/bash",
+                    FileName = "bash",
+                    Arguments = $"-c \"{escapedArgs}\"",
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true,  
+                }
+            };
+            process.Start();
+            string result = process.StandardOutput.ReadToEnd();
+            process.WaitForExit();
+            return result;
+        }
+    }
+}
