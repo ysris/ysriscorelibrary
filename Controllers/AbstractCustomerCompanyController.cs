52c5019a964f6a45530d319de7e3e3c83cbb012c
Yoann <yoann@ysris.ch>
2018-06-08 23:01:11 +0200

    Added customer company logic

---
+++ Controllers/AbstractCustomerCompanyController.cs
@@ -0,0 +1,155 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ysriscorelibrary.Interfaces;
+using YsrisCoreLibrary.Models;
+using YsrisCoreLibrary.Services;
+
+namespace YsrisCoreLibrary.Controllers
+{
+    public class AbstractCustomerCompanyController<T, U> : AbstractController<T> where T : class, IAbstractEntity, ICustomerCompany, new() where U : class, ICustomer
+    {
+        protected readonly IStorageService _storage;
+        protected readonly SessionHelperService<Customer> _session;
+        protected readonly IHostingEnvironment _env;
+        protected readonly StripeService _stripe;
+        protected readonly IConfiguration _configuration;
+        protected readonly ILogger<T> _log;
+
+        #region Constructors
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="session"></param>
+        /// <param name="storageService"></param>
+        public AbstractCustomerCompanyController(
+            DbContext context,
+            SessionHelperService<Customer> session,
+            IStorageService storageService,
+            IHostingEnvironment env,
+            StripeService stripe,
+            IConfiguration configuration,
+            ILogger<T> log
+        ) : base(context)
+        {
+            _storage = storageService;
+            _session = session;
+            _env = env;
+            _stripe = stripe;
+            _configuration = configuration;
+            _log = log;
+        }
+        #endregion
+
+        /// <summary>
+        /// Get connected user account action
+        /// </summary>
+        /// <returns>Customer</returns>
+        [HttpGet("me")]
+        [Authorize(AuthenticationSchemes = "Bearer, Cookies")]
+        public virtual async Task<IActionResult> GetMe()
+        {
+            return Ok(await _getMe());
+        }
+
+        protected virtual async Task<T> _getMe()
+        {
+            if (_session.User == null)
+                return null;
+            var entity = await _context.Set<T>().FindAsync(_session.User.companyId);
+            if ((entity).creatorCustomerId != null)
+                entity.creatorustomer = await _context.Set<U>().FindAsync(entity.creatorCustomerId); //todo : move upper in
+
+            if (entity != null)
+                entity.pictureClientAccessor = $"/api/customercompany/avatar/{entity.id}";
+
+            return entity;
+        }
+
+
+        /// <summary>
+        /// Get connected user avatar file content action
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("avatar")]
+        [Authorize(AuthenticationSchemes = "Bearer, Cookies")]
+        public virtual async Task<IActionResult> GetAvatar()
+        {
+            return await GetAvatar((int)_session.User.companyId);
+        }
+
+        /// <summary>
+        /// Get any user avatar file content action
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("avatar/{id}")]
+        [Authorize(AuthenticationSchemes = "Bearer, Cookies")]
+        public virtual async Task<IActionResult> GetAvatar(int id)
+        {
+            var path = Path.Combine(_env.WebRootPath, "bobos_components/assets/images/profile-placeholder.png");
+
+            var entity = await _context.Set<CustomerCompany>().FindAsync(id);
+            if (entity?.picture != null)
+                path = entity.picture;
+
+            if (entity.picture == null)
+                return File(System.IO.File.ReadAllBytes(path), "image/png");
+
+            try
+            {
+                var result = _storage.GetFileContent(entity.picture)?.Result?.ToArray();
+                if (result == null)
+                    return File(System.IO.File.ReadAllBytes(path), "image/png");
+                return File(result, "image/jpeg");
+            }
+            catch
+            {
+                return File(System.IO.File.ReadAllBytes(path), "image/png");
+            }
+        }
+
+        /// <summary>
+        /// Upload avatar of connected user
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost("avatar")]
+        [Authorize(AuthenticationSchemes = "Bearer, Cookies", Policy = "CompanyAdministrator")]
+        public virtual async Task<CustomerCompany> UploadAvatar(IFormFile file)
+        {
+            _log.LogInformation($"+ UploadAvatar file={file}");
+
+            var largePath = $"/companyavatars/large/{_session.User.companyId}.jpg";
+            _storage.SavePictureTo(file, largePath, 300);
+
+            var entity = await _context.Set<CustomerCompany>().FindAsync(_session.User.companyId);
+            entity.picture = largePath;
+
+            _context.Set<CustomerCompany>().Update(entity);
+            await _context.SaveChangesAsync();
+
+            return entity;
+        }
+
+
+        [Authorize(AuthenticationSchemes = "Bearer, Cookies", Policy = "CompanyAdministrator")]
+        public override Task<IActionResult> Patch([FromBody] T values)
+        {
+            var entity = _context.Set<T>().Find(_session.User.companyId);
+            entity.SetFromValues(values);
+            return base.Patch(entity);
+        }
+
+    }
+}
---
+++ Models/CustomerCompany.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using ysriscorelibrary.Interfaces;
+using YsrisCoreLibrary.Models.Stripe;
+
+namespace YsrisCoreLibrary.Models
+{
+    [DataContract]
+    public class CustomerCompany : IAbstractEntity, ICustomerCompany
+    {
+        public CustomerCompany()
+        {
+
+        }
+
+        public CustomerCompany(dynamic values)
+        {
+            SetFromValues((IAbstractEntity)values);
+        }
+
+        public void SetFromValues(IAbstractEntity values)
+        {
+            var cast = ((CustomerCompany)values);
+
+
+            if (cast.id != null) id = cast.id;
+            if (cast.name != null) name = cast.name;
+            if (cast.phoneNumber != null) phoneNumber = cast.phoneNumber;
+            if (cast.corporateEmail != null) corporateEmail = cast.corporateEmail;
+            if (cast.vat != null) vat = cast.vat;
+            if (cast.creationDate != null) creationDate = cast.creationDate;
+            if (cast.deletionDate != null) deletionDate = cast.deletionDate;
+            if (cast.creatorCustomerId != null) creatorCustomerId = cast.creatorCustomerId;
+            if (cast.adrLine1 != null) adrLine1 = cast.adrLine1;
+            if (cast.adrPostalCode != null) adrPostalCode = cast.adrPostalCode;
+            if (cast.adrCity != null) adrCity = cast.adrCity;
+            if (cast.adrCountry != null) adrCountry = cast.adrCountry;
+            if (cast.picture != null) picture = cast.picture;
+            if (cast.pictureClientAccessor != null) pictureClientAccessor = cast.pictureClientAccessor;
+            if (cast.subscribedcustomersCount != null) subscribedcustomersCount = cast.subscribedcustomersCount;
+            if (cast.billedAmount != null) billedAmount = cast.billedAmount;
+            if (cast.availableLicenceCount != null) availableLicenceCount = cast.availableLicenceCount;
+            if (cast.totalLicenceCount != null) totalLicenceCount = cast.totalLicenceCount;
+        }
+
+
+        /// <summary>
+        /// Primary key, AUTO_INCREMENT
+        /// </summary>
+        [DataMember]
+        [Key]
+        public int id { get; set; }
+
+        [DataMember]
+        public string name { get; set; }
+
+        [DataMember]
+        public string phoneNumber { get; set; }
+
+        [DataMember]
+        public string corporateEmail { get; set; }
+
+        [DataMember]
+        public string vat { get; set; }
+
+        [DataMember]
+        public DateTime? creationDate { get; set; }
+        [DataMember]
+        public DateTime? deletionDate { get; set; }
+
+        [DataMember]
+        public int? creatorCustomerId { get; set; }
+
+        [DataMember]
+        [NotMapped]
+        public ICustomer creatorustomer { get; set; }
+
+        [DataMember]
+        public string adrLine1 { get; set; }
+        [DataMember]
+        public string adrPostalCode { get; set; }
+        [DataMember]
+        public string adrCity { get; set; }
+        [DataMember]
+        public string adrCountry { get; set; }
+
+        [DataMember]
+        public string picture { get; set; }
+
+        [NotMapped]
+        [DataMember]
+        public string prettyName => name;
+
+        [DataMember]
+        [NotMapped]
+        public string pictureClientAccessor { get; set; }
+
+        [DataMember]
+        [NotMapped]
+        public CustomerCompanyStripeSubscription subscription { get; set; }
+
+        [DataMember]
+        [NotMapped]
+        public List<Customer> subscribedcustomers { get; set; }
+
+        [DataMember]
+        [NotMapped]
+        public int subscribedcustomersCount { get; set; }
+
+        [DataMember]
+        [NotMapped]
+        public object billedAmount { get;  set; }
+
+        [DataMember]
+        [NotMapped]
+        public int availableLicenceCount { get; set; }
+
+        [DataMember]
+        [NotMapped]
+        public int totalLicenceCount { get; set; }
+    }
+}
