291ae87b0c27ce06df7d62c95f683dffa994ed76
Yoann <yoann@ysris.ch>
2018-06-08 23:02:59 +0200

    Added customer company interface

---
+++ Models/ICustomerCompany.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YsrisCoreLibrary.Models
+{
+    public interface ICustomerCompany
+    {
+        int? creatorCustomerId { get; }
+        ICustomer creatorustomer { get; set; }
+        string pictureClientAccessor { get; set; }
+    }
+}
