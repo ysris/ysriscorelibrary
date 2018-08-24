bb22d17d4664d9cea2a3e39d4db92ba14798606b
Yoann <yoann@ysris.ch>
2018-08-24 23:45:44 +0200

    Add winbiz exception

---
+++ Exceptions/WinBizException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace YsrisCoreLibrary.Exceptions
+{
+    public class WinBizException : Exception
+    {
+        public WinBizException(string s) : base(s)
+        {
+        }
+    }
+}
